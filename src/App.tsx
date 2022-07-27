03ab3fe77feb0d5b75fefa233507376ffff21bb0
aKirpichenok <a.kirpichenok99@gmail.com>
2022-07-27 19:03:14 +0300

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="wrapper">
+      <div className="_container">
+        <h1 className="title">todos</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
